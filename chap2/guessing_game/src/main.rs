ff87120f3951d3b4e514ced540282336eceb731c
白井優武 <masatakeshirai0814@gmail.com>
2020-04-24 14:04:14 +0900

    数あてゲーム完成

--- chap2/guessing_game/src/main.rs
+++ chap2/guessing_game/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("The secret number is {}", secret_number);
+    //println!("The secret number is {}", secret_number);
 
     loop{
         println!("Please input your guess.");
@@ -20,13 +20,16 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Faild to read line");
         
+        //"parse" is rarse strings to number. 
+        //"u32" is 32-bit non-negative integer
         let guess: u32 = match guess.trim().parse(){
             Ok(num) => num,
+            //match all Error
             Err(_) => continue,
         };
-    
+        
         println!("You guessed:{}",guess);
-    
+        
         match guess.cmp(&secret_number){
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
