33021855f7262374d2e98c4d3493ca912f2d3a23
白井優武 <masatakeshirai0814@gmail.com>
2020-04-24 12:37:55 +0900

    乱数を出力する

--- chap2/guessing_game/src/main.rs
+++ chap2/guessing_game/src/main.rs
@@ -1,9 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is {}", secret_number);
+
     println!("Please input your guess.");
-    
+
     //"let" is variable definition,
     //"mut" is make variable mutable
     let mut guess = String::new(); 
@@ -11,5 +16,5 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Faild to read line");
     
-        println!("You guessed:{}",guess)
+        println!("You guessed:{}",guess);
 }
