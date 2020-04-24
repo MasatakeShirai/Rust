2a80080b78532b721d543cbe46911192322655da
白井優武 <masatakeshirai0814@gmail.com>
2020-04-24 13:23:36 +0900

    入力をループする

--- chap2/guessing_game/src/main.rs
+++ chap2/guessing_game/src/main.rs
@@ -10,23 +10,25 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1,101);
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop{
+        println!("Please input your guess.");
 
-    //"let" is variable definition,
-    //"mut" is make variable mutable
-    let mut guess = String::new(); 
-
-    io::stdin().read_line(&mut guess)
-        .expect("Faild to read line");
+        //"let" is variable definition,
+        //"mut" is make variable mutable
+        let mut guess = String::new(); 
     
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-
-    println!("You guessed:{}",guess);
-
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        io::stdin().read_line(&mut guess)
+            .expect("Faild to read line");
+        
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+    
+        println!("You guessed:{}",guess);
+    
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
