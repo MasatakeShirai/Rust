9e5a8a17bb6e0d6855ece4d07da7b85d3adc63ac
白井優武 <masatakeshirai0814@gmail.com>
2020-04-25 15:50:53 +0900

    関数の返り値

--- chap3/functions/src/main.rs
+++ chap3/functions/src/main.rs
@@ -9,9 +9,23 @@ fn main() {
     };
     
     println!("x:{}, y:{}",x,y);
+
+    let num = five();
+    println!("num:{}",num);
+
+    let num = plus_one(5);
+    println!("num:{}",num);
 }
 
 fn another_function(x:i32, y:i32){
     println!("another function");
     println!("The value is x:{}, y:{}",x,y);
+}
+
+fn five() -> i32{
+    5
+}
+
+fn plus_one(x: i32) -> i32{
+    x + 1
 }
