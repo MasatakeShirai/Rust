60d0102f91301404ed81c435d3a53f1aa28c62f3
白井優武 <masatakeshirai0814@gmail.com>
2020-04-25 16:15:06 +0900

    else if式

--- chap3/branches/src/main.rs
+++ chap3/branches/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    let number = 7;
+    let number = 5;
 
     if number < 5 {
         println!("condition was true");
+    } else if number == 5{
+        println!("Equal");
     } else {
         println!("condition was falth");
     }
