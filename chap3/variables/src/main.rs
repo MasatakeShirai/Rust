33cf83a100592080545a66214bc6bd56ece0caa0
白井優武 <masatakeshirai0814@gmail.com>
2020-04-25 12:24:26 +0900

    シャドーイング

--- chap3/variables/src/main.rs
+++ chap3/variables/src/main.rs
@@ -8,4 +8,10 @@ fn main() {
     //Differences between variables and constants
     const MAX_POINTS: u32 = 100_000;
     println!("MAX_POINT is {}",MAX_POINTS);
+
+    //Shadowing
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!{"The value of y is {}",y}
 }
