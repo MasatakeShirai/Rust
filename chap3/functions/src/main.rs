08c97126cacd480eea3aa5ba535c2b41cc2b0734
白井優武 <masatakeshirai0814@gmail.com>
2020-04-25 15:34:26 +0900

    文と式

--- chap3/functions/src/main.rs
+++ chap3/functions/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
     println!("Hello, world!");
     another_function(5,10);
+
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    
+    println!("x:{}, y:{}",x,y);
 }
 
 fn another_function(x:i32, y:i32){
