b7781d65b6c8c0e011b297d42657e18c614f174d
白井優武 <masatakeshirai0814@gmail.com>
2020-04-25 15:23:35 +0900

    関数の引数

--- chap3/functions/src/main.rs
+++ chap3/functions/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function(5,10);
 }
 
-fn another_function(){
+fn another_function(x:i32, y:i32){
     println!("another function");
+    println!("The value is x:{}, y:{}",x,y);
 }
