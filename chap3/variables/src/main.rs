cfb82059865c08bcc56355cbf2fbe8e32ec423f9
白井優武 <masatakeshirai0814@gmail.com>
2020-04-25 12:20:45 +0900

    変数と定数の違い

--- chap3/variables/src/main.rs
+++ chap3/variables/src/main.rs
@@ -1,8 +1,11 @@
-//Variables and mutability
-
 fn main() {
+    //Variables and mutability
     let mut x = 5;
     println!{"The value of x is {}",x}
     x = 6;
     println!{"The value of x is {}",x}
+
+    //Differences between variables and constants
+    const MAX_POINTS: u32 = 100_000;
+    println!("MAX_POINT is {}",MAX_POINTS);
 }
