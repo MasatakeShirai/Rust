e7e6744389308dae1671b738ef94f2a4d4aa6778
白井優武 <masatakeshirai0814@gmail.com>
2020-04-25 12:13:54 +0900

    変数と可変性

--- chap3/variables/src/main.rs
+++ chap3/variables/src/main.rs
@@ -1,5 +1,8 @@
 //Variables and mutability
 
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!{"The value of x is {}",x}
+    x = 6;
+    println!{"The value of x is {}",x}
 }
