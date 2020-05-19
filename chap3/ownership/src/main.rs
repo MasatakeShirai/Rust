837e398cbc53536d86ca11da9e77d338b5c5bf74
白井優武 <masatakeshirai0814@gmail.com>
2020-05-19 23:53:20 +0900

    clone

--- chap3/ownership/src/main.rs
+++ chap3/ownership/src/main.rs
@@ -3,7 +3,13 @@ fn main() {
     s.push_str(", world!");
     println!("{}",s);
 
+    //move(Shallow copy)
     let s1 = String::from("hello");
     let s2 = s1;
     println!("{}",s2);
+
+    //clone(Deep copy)
+    let s1 = String::from("Hay");
+    let s2 = s1.clone();
+    println!("{},{}",s1,s2);
 }
