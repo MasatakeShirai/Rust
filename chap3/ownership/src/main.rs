5d3ec0e349d2de35902a3649d4b91cc3cf8866e7
白井優武 <masatakeshirai0814@gmail.com>
2020-05-19 23:48:40 +0900

    move

--- chap3/ownership/src/main.rs
+++ chap3/ownership/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     let mut s = String::from("hello");
     s.push_str(", world!");
-    println!("{}",s)
+    println!("{}",s);
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}",s2);
 }
