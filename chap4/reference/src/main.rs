7dfae3b34cf85132140c8f1c9e7a0fdd1198bdf2
白井優武 <masatakeshirai0814@gmail.com>
2020-05-20 16:07:54 +0900

    可変な参照

--- chap4/reference/src/main.rs
+++ chap4/reference/src/main.rs
@@ -1,11 +1,18 @@
 fn main() {
     let s1 = String::from("hello");
     let len = calculate_length(&s1);
-    println!("The length of {} is {}.",s1, len)    
+    println!("The length of {} is {}.",s1, len);    
 
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{}",s);
 }
 
 fn calculate_length(s:&String)->usize{
     //s is refernce to s string
     s.len()
+}
+
+fn change(some_string:&mut String){
+    some_string.push_str(",world")
 }
