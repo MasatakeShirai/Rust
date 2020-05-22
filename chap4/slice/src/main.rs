d00415c722d98cbd177acd9899077f2a8832743a
白井優武 <masatakeshirai0814@gmail.com>
2020-05-22 23:39:13 +0900

    文字列スライス

--- chap4/slice/src/main.rs
+++ chap4/slice/src/main.rs
@@ -1,17 +1,52 @@
 fn main() {
     let mut s = String::from("hello world");
-    let word = first_word(&s);
+    let fword = first_word(&s);
+    let sword = second_word(&s);
+    
+    println!("{},{}",fword,sword);
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("{} {}",hello,world);
+    
+    s = String::from("hello");
+
+    let mut slice1 = &s[0..2];
+    let mut slice2 = &s[..2];
+    println!("{}, {}",slice1,slice2);
+    
+    let len = s.len();
+
+    slice1 = &s[3..len];
+    slice2 = &s[3..];
+    println!("{}, {}",slice1,slice2);
+
+    slice1 = &s[0..len];
+    slice2 = &s[..len];
+    println!("{}, {}",slice1,slice2);    
+    
     s.clear();
-    println!("{}",word);
 }
 
-fn first_word(s:&String)->usize{
+fn first_word(s:&String)->&str{
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate(){
         if item == b' '{
-            return i;
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
+}
+
+fn second_word(s:&String)->&str{
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &s[i..];
+        }    
+    } 
+    &s[..]
 }
