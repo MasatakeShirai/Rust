8ebbe89e18ce6ae45702bf9093dea13fed2c0e2e
白井優武 <masatakeshirai0814@gmail.com>
2020-05-25 12:46:51 +0900

    構造体とそのインスタンス化

---
+++ chap5/structure/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    let mut user1 = User{
+        email: String::from("someone@example.ac.jp"),
+        username: String::from("SomeoneName"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    print_user(&user1);
+
+    user1.email = String::from("another@example.ac.jp");
+
+    print_user(&user1);
+
+    let user2 = buile_user(String::from("user2@ac.jp",), String::from("user2"));
+    print_user(&user2);
+}
+
+struct User{
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn print_user(user:&User){
+    println!("-------------------");
+    print!("username:{}\nemail:{}\nsign_in_count:{}\nactive:{}\n"
+            ,user.username,user.email,user.sign_in_count,user.active);
+    println!("-------------------");
+}
+
+fn buile_user(email:String, name:String)->User{
+    User{
+        email: email,
+        username: name,
+        active: true,
+        sign_in_count: 1,
+    }
+}
