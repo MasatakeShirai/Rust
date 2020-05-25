83a5e223d79318a7efbc4961273de9d8cdf0587f
白井優武 <masatakeshirai0814@gmail.com>
2020-05-25 13:44:53 +0900

    トレイト継承で機能追加

--- chap5/rectangles/src/main.rs
+++ chap5/rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle{
     width: u32,
     height: u32,
@@ -5,6 +6,8 @@ struct Rectangle{
 
 fn main() {
     let lect1 = Rectangle{width:30, height:50};
+
+    println!("Rect1 is {:#?}",lect1);
 
     println!(
         "The area of the rectangle is {} square pixels."
