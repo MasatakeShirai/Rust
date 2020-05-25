a52dac06a3d90ef7aa2ebbeee6132cef029daa09
白井優武 <masatakeshirai0814@gmail.com>
2020-05-25 13:27:05 +0900

    構造体でリファクタリング

--- chap5/rectangles/src/main.rs
+++ chap5/rectangles/src/main.rs
@@ -1,12 +1,17 @@
+struct Rectangle{
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let lect1 = (30,50);
+    let lect1 = Rectangle{width:30, height:50};
 
     println!(
         "The area of the rectangle is {} square pixels."
-        ,area(lect1)
+        ,area(&lect1)
     );
 }
 
-fn area(dimensions: (u32,u32))->u32{
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle)->u32{
+    rectangle.height * rectangle.width
 }
