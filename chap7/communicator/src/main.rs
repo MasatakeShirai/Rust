50126fd847896e41a6acf633539f00a92a0b203e
白井優武 <masatakeshirai0814@gmail.com>
2020-06-02 14:19:22 +0900

    useキーワードの利用

--- chap7/communicator/src/main.rs
+++ chap7/communicator/src/main.rs
@@ -1,5 +1,29 @@
 extern crate communicator;
 
+pub mod a{
+    pub mod series{
+        pub mod of{
+            pub fn nested_modules(){}
+        }
+    }
+}
+
+enum TrafficLight{
+    Red,
+    Yellow,
+    Green,
+}
+
+use a::series::of::nested_modules;
+use TrafficLight::{Red,Yellow};
+
 fn main(){
     communicator::client::connect();
+
+    a::series::of::nested_modules();
+    nested_modules();
+
+    let red = Red;
+    let yellow = Yellow;
+    let green = TrafficLight::Green;
 }
