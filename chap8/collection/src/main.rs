49fb45f660c256ca6d956258618e6d277bc1ed6e
白井優武 <masatakeshirai0814@gmail.com>
2020-06-03 14:16:25 +0900

    HashMapの生成

--- chap8/collection/src/main.rs
+++ chap8/collection/src/main.rs
@@ -59,4 +59,16 @@ fn main() {
     let s = format!("{}-{}-{}",s1,s2,s3);
     println!("{}",s);        
     
+    //HashMap
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Red"),String::from("Green")];
+    let initial_scores = vec![10,50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 }
