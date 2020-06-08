e2a2f057b9863a763be91f2f5e0ff3d1a7c38b8a
白井優武 <masatakeshirai0814@gmail.com>
2020-06-08 12:44:16 +0900

    Resultで回復可能なエラー

--- chap9/error/src/main.rs
+++ chap9/error/src/main.rs
@@ -1,7 +1,17 @@
+use std::fs::File;
+
 fn main() {
     //panic!("crach and burn!");
 
     //let v = vec![1,2,3];
     //v[99];
 
+    let f = File::open("hello.txt");
+
+    let f = match f{
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a ploblem oping the file: {:?}",error)
+        },
+    };
 }
